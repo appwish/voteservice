5c4c25cdca86f67b10aea2381d9388b36896b2a8
Jorge <jlpnunes@gmail.com>
2020-02-01 18:38:39 -0500

    Adding project structure

---
+++ docker-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t appwish/voteservice . && docker push appwish/voteservice
---
+++ src/main/java/oi/appwish/voteservice/model/Vote.java
@@ -0,0 +1,118 @@
+package oi.appwish.voteservice.model;
+
+import io.appwish.grpc.VoteProto;
+import java.util.Objects;
+import net.badata.protobuf.converter.annotation.ProtoClass;
+import net.badata.protobuf.converter.annotation.ProtoField;
+
+/**
+ * {@link ProtoClass} and {@link ProtoField} annotations are used by {@link
+ * net.badata.protobuf.converter.Converter} to convert back/forth between protobuf data transfer
+ * objects and model objects.
+ *
+ * The converter requires a POJO with getters, setters and a default constructor.
+ */
+@ProtoClass(VoteProto.class)
+public class Vote {
+
+  @ProtoField
+  private long id;
+
+  @ProtoField
+  private String title;
+
+  @ProtoField
+  private String content;
+
+  @ProtoField
+  private String coverImageUrl;
+
+  @ProtoField
+  private long authorId;
+
+  @ProtoField
+  private String url;
+
+  public Wish(final long id, final String title, final String content, final String coverImageUrl, final long authorId, final String url) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.coverImageUrl = coverImageUrl;
+    this.authorId = authorId;
+    this.url = url;
+  }
+
+  public Wish() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public void setCoverImageUrl(String coverImageUrl) {
+    this.coverImageUrl = coverImageUrl;
+  }
+
+  public String getCoverImageUrl() {
+    return coverImageUrl;
+  }
+
+  public long getAuthorId() {
+    return authorId;
+  }
+
+  public void setAuthorId(long authorId) {
+    this.authorId = authorId;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Wish wish = (Wish) o;
+    return id == wish.id &&
+      authorId == wish.authorId &&
+      title.equals(wish.title) &&
+      content.equals(wish.content) &&
+      Objects.equals(coverImageUrl, wish.coverImageUrl) &&
+      url.equals(wish.url);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, title, content, coverImageUrl, authorId, url);
+  }
+}
+
